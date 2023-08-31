81a2c209d0cf68f9c72bd2bd3b04e10966198c4c
ikoshkila <114185021+ikoshkila@users.noreply.github.com>
2023-08-30 14:56:52 -1000

    Add files via upload

---
+++ main.py
@@ -0,0 +1,14 @@
+def sum(a,b):
+    return a + b
+
+def minus(a,b):
+    return a - b
+
+
+a = int(input())
+b = int(input())
+print(sum(a,b))
+
+a = int(input())
+a = int(input())
+print(minus(a,b))
